6203d294c44cd86bea8f96cb7b9c88b098b6debf
Max Marchuk <themaxmarchuk@gmail.com>
2021-12-08 21:28:51 -0600

    Add button animations

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App () {
         <span>Goal</span>
         <span className='text-green-bright ml-1'>App</span>
       </div>
-      <div className='flex mt-16'>
+      <div className='flex justify-between mt-16'>
         <div className='text-[90px] ml-40 relative inline-block'>
           <h1>Track your Goals</h1>
           <h1 className='text-right italic'>
@@ -23,27 +23,27 @@ export default function App () {
           <img src={rocket} className='scale-[85%] absolute left-[1.5rem] top-[4.5rem]' />
           <img src={smoke} className=' scale-125 z-[-1] absolute left-[-11rem] bottom-[-11rem]' />
         </div>
-        <div className='inline-block text-3xl ml-[11.5rem] mt-52'>
+        <div className='inline-block text-3xl mr-[8vw] mt-52'>
           <div>
-           Visualize your journy. 
+            Visualize your journey.
           </div>
-          <div className='bg-blue-base mt-3 w-[47rem] h-[9rem] rounded-md' />
+          <div className='bg-blue-base mt-3 w-[47rem] h-[9rem] rounded-md shadow-lg' />
           <div className='mt-24'>
-            Progress redifined.<br/>
+            Progress redefined.<br />
             Based on proven psychological principles.
           </div>
           <div className='mt-12 flex'>
             <button className='text-xl text-center'>
-              <div className='flex border-2 relative bg-blue-dark border-blue-light justify-center z-10 items-center pt-4 pb-4 w-[13.5rem] rounded-[10px]'>
+              <div className='flex border-2 relative transition-all active:translate-y-1 hover:translate-y-[-10px] bg-blue-dark border-blue-light justify-center z-10 items-center pt-4 pb-4 w-[13.5rem] rounded-[10px]'>
                 See More
               </div>
-              <div className='flex bg-blue-light relative translate-y-[-49px] z-0 h-[3.75rem] w-[13.5rem] rounded-[10px]'/>
+              <div className='flex bg-blue-light shadow-md relative translate-y-[-49px] z-0 h-[3.75rem] w-[13.5rem] rounded-[10px]' />
             </button>
-            <button className='text-xl text-center ml-8'>
-              <div className='flex relative bg-magenta-bright justify-center z-10 items-center pt-4 pb-4 translate-y-[-2px] w-[13.5rem] rounded-[10px]'>
+            <button className='text-xl text-center ml-8' >
+              <div className='flex relative bg-magenta-bright justify-center z-10 items-center pt-4 pb-4 transition-all hover:translate-y-[-12px] active:translate-y-1 translate-y-[-2px] w-[13.5rem] rounded-[10px]'>
                 Launch Today <img src={faRocket} className='scale-[85%] ml-[11px]' />
               </div>
-              <div className='flex bg-purple-dark relative translate-y-[-47px] z-0 h-[3.75rem] w-[13.5rem] rounded-[10px]'/>
+              <div className='flex bg-purple-dark shadow-md relative translate-y-[-47px]  z-0 h-[3.75rem] w-[13.5rem] rounded-[10px]' />
             </button>
           </div>
         </div>
